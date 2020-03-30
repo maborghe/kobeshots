3b9fbb444f3771772584aef026a7bb0da4add16d
Matteo Borghesi <ma.borghe@gmail.com>
2020-03-30 17:50:16 +0200

    Add shot_angle and shot_side

--- kobe.R
+++ kobe.R
@@ -8,12 +8,15 @@ attach(kobe)
 
 # prune columns
 time_remaining = minutes_remaining*60+seconds_remaining
-kobe = cbind(kobe, time_remaining)
-rm(time_remaining)
+feet_x = abs(loc_x)/10
+feet_y = loc_y/10
+kobe$shot_distance = sqrt(feet_x^2 + feet_y^2)
+shot_angle = atan2(feet_y, feet_x)
+shot_side = ifelse(loc_x >= 0, "Right", "Left")
+kobe = cbind(kobe, time_remaining, shot_angle, feet_x, feet_y, shot_side)
 kobe = kobe[,-which(names(kobe) %in% c("minutes_remaining","seconds_remaining","team_name",
-                                       "team_id", "matchup", "game_event_id", "game_id", "game_date", "shot_id"))]
-
-fix(kobe)
+                                      "team_id", "matchup", "game_event_id", "game_id",
+                                      "game_date", "shot_id", "lat", "lon", "loc_x", "loc_y"))]
 
 unique(combined_shot_type)
 unique(action_type)
@@ -24,7 +27,6 @@ probs = predict(logreg, type="response")
 preds = (probs>.45)
 table(shot_made_flag, preds)
 summary(logreg)
-fix(kobe)
 summary(shot_distance)
 summary(loc_y)
 summary(loc_x)
@@ -35,5 +37,7 @@ madeShots = which(shot_made_flag==TRUE)
 dotsCol[madeShots] = "blue"
 rm(madeShots)
 plot(loc_y, loc_x, type="p", cex=0.1, pch=20, col=dotsCol)
-hist(shot_distance)
+#hist(shot_distance)
 
+detach(kobe)
+rm(list=ls())
